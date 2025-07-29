5812f9a5e7865cb81140b991a64bfaef8eaf9723
Luca <171629213+LucaX877@users.noreply.github.com>
2025-07-29 19:35:04 +0100

    Add onmousemove event to draw in grid

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    if (i % 4 == 0) {
-        container.appendChild(document.createElement("div"));
-        container.lastChild.style.borderLeft = "4px solid black";
-    } else if (i % 4 == 3) {
+for (let i = 0; i < 256; i++) {
+    if (i % 16 == 15) {
         container.appendChild(document.createElement("div"));
         container.lastChild.style.borderRight = "4px solid black";
-    }
-    else container.appendChild(document.createElement("div"));
+    } else if (i % 16 == 0) {
+        container.appendChild(document.createElement("div"));
+        container.lastChild.style.borderLeft = "4px solid black";
+    } else container.appendChild(document.createElement("div"));
 }
+
+const squares = container.querySelectorAll("div");
+
+for (let square of squares) {
+    square.addEventListener("mousemove", () => {
+        square.classList.add("hit");
+    })
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    for (let square of squares) {
+        square.remove();
+    }}
+)
