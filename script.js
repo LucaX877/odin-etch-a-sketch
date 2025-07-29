9c6ca25a3bad792a34397b73964b747694730e12
Luca <171629213+LucaX877@users.noreply.github.com>
2025-07-29 21:10:25 +0100

    Add reset button

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-    if (i % 16 == 15) {
-        container.appendChild(document.createElement("div"));
-        container.lastChild.style.borderRight = "4px solid black";
-    } else if (i % 16 == 0) {
-        container.appendChild(document.createElement("div"));
-        container.lastChild.style.borderLeft = "4px solid black";
-    } else container.appendChild(document.createElement("div"));
+function drawGrid(number) {
+    for (let i = 0; i < number * number; i++) {
+        if (i % number == number - 1) {
+            container.appendChild(document.createElement("div"));
+            container.lastChild.style.borderRight = "2px solid black";
+        } else if (i % number == 0) {
+            container.appendChild(document.createElement("div"));
+            container.lastChild.style.borderLeft = "2px solid black";
+        } else container.appendChild(document.createElement("div"));
+        container.lastChild.style.height = 640/number + "px";
+        container.lastChild.style.width = 640/number + "px";
+    }
+    squares = drawingEvent();
+    return squares;
 }
 
-const squares = container.querySelectorAll("div");
+function drawingEvent() {
+    let squares = container.querySelectorAll("div");
 
-for (let square of squares) {
-    square.addEventListener("mousemove", () => {
-        square.classList.add("hit");
-    })
+    for (let square of squares) {
+        square.addEventListener("mousemove", () => {
+            square.classList.add("hit");
+        })
+    }
+    return squares
 }
 
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
     for (let square of squares) {
         square.remove();
-    }}
-)
+    }
+    alert("Max number is 70");
+    let number = Math.min(parseInt(prompt("Enter number: ")),70)
+    squares = drawGrid(number);
+})
+
+squares = drawGrid(16);
